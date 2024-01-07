2c3c9eaf1dd8cec8517361612c5628ef800f9692
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-01-07 18:44:20 +0100

    Dependencies injected
    (cherry picked from commit 9ea4a4f787fb0884d723f197ae650000a3ace8ec)

--- app/src/main/java/ru/otus/di/MainViewModel.kt
+++ app/src/main/java/ru/otus/di/MainViewModel.kt
@@ -2,7 +2,8 @@ package ru.otus.di
 
 import android.app.Application
 import android.util.Log
-import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
@@ -11,15 +12,15 @@ import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import ru.otus.di.domain.dao.EmployeeDao
 import ru.otus.di.domain.data.Data
 import ru.otus.di.domain.data.Employee
+import ru.otus.di.domain.net.EmployeeService
 
-class MainViewModel(application: Application) : AndroidViewModel(application) {
-    // Зависимость 2
-    private val employeesDao = (application as MyDiApplication).employeeDao
-
-    // Зависимость 3
-    private val network = (application as MyDiApplication).net
+class MainViewModel(
+    private val employeesDao: EmployeeDao,
+    private val network: EmployeeService
+) : ViewModel() {
 
     val data: StateFlow<Data<Employee>?> = employeesDao
         .getList()
@@ -44,5 +45,15 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     companion object {
         private const val TAG = "MainViewModel"
+    }
+
+    class Factory(application: Application) : ViewModelProvider.Factory {
+        private val diApp = application as MyDiApplication
+
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T = MainViewModel(
+            diApp.employeeDao,
+            diApp.net
+        ) as T
     }
 }
