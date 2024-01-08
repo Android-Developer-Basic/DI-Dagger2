453b3e84082668c93324cc875df71fa3aa731428
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-01-08 17:11:49 +0100

    Application component
    (cherry picked from commit 4d6be299203b388f9287cfab0525c0ddda044d79)

--- app/src/main/java/ru/otus/di/MyDiApplication.kt
+++ app/src/main/java/ru/otus/di/MyDiApplication.kt
@@ -1,41 +1,7 @@
 package ru.otus.di
 
 import android.app.Application
-import android.content.Context
-import ru.otus.di.domain.dao.EmployeeDao
-import ru.otus.di.domain.dao.EmployeeDetailsDao
-import ru.otus.di.domain.net.EmployeeService
-import ru.otus.di.net.EmployeeServiceImpl
 
 class MyDiApplication : Application() {
-    val component: AppComponent by lazy {
-        ManualAppComponent.Factory.build(this)
-    }
-}
-
-class ManualAppComponent private constructor(
-    private val employeeDaoProvider: () -> EmployeeDao,
-    private val employeeDetailsDaoProvider: () -> EmployeeDetailsDao,
-    private val employeeServiceProvider: () -> EmployeeService
-) : AppComponent {
-    override fun employeeDao(): EmployeeDao = employeeDaoProvider()
-
-    override fun employeeDetailsDao(): EmployeeDetailsDao = employeeDetailsDaoProvider()
-
-    override fun employeeService(): EmployeeService = employeeServiceProvider()
-
-    object Factory : AppComponent.Factory {
-        override fun build(context: Context): AppComponent {
-            // Зависимости DB
-            val dbModule = DbModule()
-            // Singlton
-            val db = dbModule.db(context)
-
-            return ManualAppComponent(
-                { dbModule.employeeDao(db) },
-                { dbModule.employeeDetailsDao(db) },
-                { EmployeeServiceImpl() }
-            )
-        }
-    }
+    val component: AppComponent = DaggerAppComponent.factory().build(this)
 }
