28c2d6113cd8b053d64afe2d29759e4b0c4c058c
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-01-11 06:44:28 +0100

    Building component manually

--- app/src/main/java/ru/otus/di/MyDiApplication.kt
+++ app/src/main/java/ru/otus/di/MyDiApplication.kt
@@ -1,25 +1,41 @@
 package ru.otus.di
 
 import android.app.Application
-import androidx.room.Room
-import ru.otus.di.db.EmployeeDb
+import android.content.Context
 import ru.otus.di.domain.dao.EmployeeDao
 import ru.otus.di.domain.dao.EmployeeDetailsDao
 import ru.otus.di.domain.net.EmployeeService
 import ru.otus.di.net.EmployeeServiceImpl
 
 class MyDiApplication : Application() {
-    private val db: EmployeeDb by lazy {
-        Room.databaseBuilder(this, EmployeeDb::class.java,"employees")
-            .fallbackToDestructiveMigration()
-            .build()
+    val component: AppComponent by lazy {
+        ManualAppComponent.Factory.build(this)
     }
+}
 
-    val net: EmployeeService by lazy {
-        EmployeeServiceImpl()
+class ManualAppComponent private constructor(
+    private val employeeDaoProvider: () -> EmployeeDao,
+    private val employeeDetailsDaoProvider: () -> EmployeeDetailsDao,
+    private val employeeServiceProvider: () -> EmployeeService
+) : AppComponent {
+    override fun employeeDao(): EmployeeDao = employeeDaoProvider()
+
+    override fun employeeDetailsDao(): EmployeeDetailsDao = employeeDetailsDaoProvider()
+
+    override fun employeeService(): EmployeeService = employeeServiceProvider()
+
+    object Factory : AppComponent.Factory {
+        override fun build(context: Context): AppComponent {
+            // Зависимости DB
+            val dbModule = DbModule()
+            // Singlton
+            val db = dbModule.db(context)
+
+            return ManualAppComponent(
+                { dbModule.employeeDao(db) },
+                { dbModule.employeeDetailsDao(db) },
+                { EmployeeServiceImpl() }
+            )
+        }
     }
-
-    val employeeDao: EmployeeDao get() = db.employees()
-
-    val employeeDetailsDao: EmployeeDetailsDao get() = db.employees()
 }
